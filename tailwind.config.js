ac01d939559226a07c274cdc3644f48039e18d9f
조유진 <uzinfamily@snu.ac.kr>
2024-10-16 12:13:20 +0900

    Merge branch 'main' of https://github.com/Joeyoojin/waffle-rookie-snutt-clone-team-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        'snutt-red': '#e44459',
-        'snutt-orange': '#f58d3d',
+        'snutt-red': '#E44459',
+        'snutt-orange': '#F58D3D',
+        'snutt-yellow': '#FAC52D',
+        'snutt-lime': '#A6D930',
+        'snutt-green': '#2BC366',
+        'snutt-lightblue': '#1BD0C9',
+        'snutt-sky': '#1D99E9',
+        'snutt-navy': '#6E66BA',
+        'snutt-purple': '#BA66BA',
       },
     },
   },
